9e3b87de4ab36c42be4c610e52b20866ce68aa3c
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2021-11-09 16:53:06 -0300

    Layout finalizado com alguns pontos faltando otimização

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
 let inputScreen = document.querySelector(".input-screen")
+let menu = document.querySelector(".menu")
+let transparentScreen = document.querySelector(".transparent-dark")
+let mainScreen = document.querySelector(".main-screen")
+let menuScreen = document.querySelector(".menu-screen")
+let absolute = document.querySelector(".absolute")
 
 function enter(){
     inputScreen.classList.add("none")
+    mainScreen.classList.remove("none")
 
-
+}
+function toggleMenu(){
+    menuScreen.classList.toggle("none")
+    absolute.classList.toggle("none")
 }
